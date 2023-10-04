347553d6f51cccc71f4dedc750c1ebb24ff8c9f6
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2023-10-04 17:33:52 +0200

    Fixed card height

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -122,36 +122,38 @@
 </div>
 
 <main class="min-h-screen flex flex-col w-full z-10">
-    <div class="w-full max-w-screen-xl mx-auto px-4 md:px-6 py-24 grow items-center justify-center grid gap-6 lg:grid-cols-3 relative" bind:this={cardContainer}>
-        {#each cards as card}
-            <div class="relative group h-full w-full">
-                <div class="absolute h-20 w-20 rounded-full {card.circleClasses}" style="opacity: 0; transition-property: all; transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1); transition-duration: 1s;"></div>
-                <!-- <span class="w-1/2 aspect-square absolute blob {card.circleClasses} transition-all duration-1000"></span> -->
-                <div class="bg-[rgba(65,65,65,0.308)] border border-[rgba(255,255,255,0.089)] relative w-full h-full flex flex-col justify-between items-center py-5 px-3 rounded-lg backdrop-blur-[30px]">
-                    <img src="https://cruip-tutorials.vercel.app/spotlight-effect/card-01.png" width="200" height="200" alt="Card 01" />
-                    <div class="grow mb-5 w-full text-center px-10">
-                        <h2 class="text-2xl text-white font-bold">{card.title}</h2>
-                        <p>{card.body}</p>
-                    </div>
-
-                    <div class="flex flex-row gap-2 flex-wrap w-full items-center justify-center">
-                        {#if card.preview}
-                            <a target="_blank" class="button" href="{card.preview}">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" /></svg>
-                                <span>Preview</span>
-                            </a>
-                        {/if}
-                        
-                        {#if card.code}
-                            <a target="_blank" class="button" href="{card.code}">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" /></svg>
-                                <span>Code</span>
-                            </a>
-                        {/if}
+    <div class="grow w-full">
+        <div class="w-full max-w-screen-xl mx-auto px-4 md:px-6 py-24 grow items-center justify-center grid gap-6 lg:grid-cols-3 relative" bind:this={cardContainer}>
+            {#each cards as card}
+                <div class="relative group w-full">
+                    <div class="absolute h-20 w-20 rounded-full {card.circleClasses}" style="opacity: 0; transition-property: all; transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1); transition-duration: 1s;"></div>
+                    <!-- <span class="w-1/2 aspect-square absolute blob {card.circleClasses} transition-all duration-1000"></span> -->
+                    <div class="bg-[rgba(65,65,65,0.308)] border border-[rgba(255,255,255,0.089)] relative w-full h-full flex flex-col justify-between items-center py-5 px-3 rounded-lg backdrop-blur-[30px]">
+                        <img src="https://cruip-tutorials.vercel.app/spotlight-effect/card-01.png" width="200" height="200" alt="Card 01" />
+                        <div class="grow mb-5 w-full text-center px-10">
+                            <h2 class="text-2xl text-white font-bold">{card.title}</h2>
+                            <p>{card.body}</p>
+                        </div>
+    
+                        <div class="flex flex-row gap-2 flex-wrap w-full items-center justify-center">
+                            {#if card.preview}
+                                <a target="_blank" class="button" href="{card.preview}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" /></svg>
+                                    <span>Preview</span>
+                                </a>
+                            {/if}
+                            
+                            {#if card.code}
+                                <a target="_blank" class="button" href="{card.code}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" /></svg>
+                                    <span>Code</span>
+                                </a>
+                            {/if}
+                        </div>
                     </div>
                 </div>
-            </div>
-        {/each}
+            {/each}
+        </div>
     </div>
 
     <footer class="w-full border-t border-[rgba(255,255,255,0.089)] px-4 py-2 text-center">
